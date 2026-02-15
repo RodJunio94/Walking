4d9f60c7b96e0a3073d80c64df04071dcff20123
Junio Rodrigues <tech.rodrigues@gmail.com>
2026-02-15 16:29:31 -0300

    Cria classe Player com movimentação e pulo
    Adiciona a classe Player ao namespace Walking, incluindo propriedades para posição, aparência e direção, além de métodos para movimentação (anda) e pulo (pula) do jogador.

---
+++ Walking/Player.cs
@@ -0,0 +1,41 @@
+﻿namespace Walking;
+
+internal class Player
+{
+    public byte Posicao { get; set; }
+    public string Aparencia { get; set; }
+    public string FacinRight { get; set; }
+    public string FacinLeft { get; set; }
+    public bool Pulando { get; set; }
+
+    public Player(string right, string left)
+    {
+        Posicao = 0;
+        Aparencia = right;
+        FacinRight = left;
+        FacinLeft = FacinRight;
+        Pulando = false;
+    }
+
+    public void pula()
+    {
+        Console.Beep(234, 100);
+    }
+
+    public void anda(bool direcao, byte limite)
+    {
+       if(direcao)
+       {
+           Posicao++;
+           Aparencia = FacinRight;
+       }
+       else
+       {
+            if(Posicao > 0)
+            {
+                Posicao--;
+                Aparencia = FacinLeft;
+            }
+        }
+    }
+}
