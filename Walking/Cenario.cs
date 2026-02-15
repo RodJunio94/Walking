85d1b86ec65703dc9d31378275b71dafb4c3823c
Junio Rodrigues <tech.rodrigues@gmail.com>
2026-02-15 16:32:52 -0300

    Adiciona classe Cenario para renderização do cenário
    Implementa a classe Cenario no namespace Walking, com propriedades para o chão e tamanho do cenário. Inclui método imprime(Player p) que desenha o cenário e o jogador no console, considerando se o jogador está pulando ou não.

---
+++ Walking/Cenario.cs
@@ -0,0 +1,48 @@
+﻿namespace Walking;
+
+internal class Cenario
+{
+    public string Chao { get; set; }
+    public byte Tamanho { get; set; }
+
+    public Cenario(string chao, byte tamanho)
+    {
+        Chao = chao;
+        Tamanho = tamanho;
+    }
+
+    public void imprime(Player p)
+    {
+        Console.Clear();
+        Console.WriteLine();
+
+        //Imprime a primeira linha
+        if(p.Pulando)
+        {
+            for (byte i = 0; i <p.Posicao; i++)
+            {
+               Console.Write(" ");
+            }
+            Console.WriteLine(p.Aparencia);
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+
+        //Imprime a segunda linha
+        for (byte i = 0; i <p.Posicao; i++)
+        {
+            Console.Write(Chao);
+        }
+
+        if (p.Pulando) Console.Write(Chao);
+        else Console.Write(p.Aparencia);
+
+        for (byte i = (byte)(p.Posicao + 1); i < Tamanho; i++)
+        {
+            Console.Write(Chao);
+        }
+
+    }
+}
